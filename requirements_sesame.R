7196fcb5c9fb300b54ffac0062557fff1a02fd8f
Patrycja Przybyłowicz <patrycja.przybylowicz@pum.edu.pl>
2025-04-02 10:19:14 +0200

    Edited requirements for SeSAME - removed unneccessary meffonym dependency managed in Docker file for R_other Docker container

--- requirements_sesame.R
+++ requirements_sesame.R
@@ -1,6 +1,6 @@
 options(Ncpus = 10)
 
-install.packages(c("devtools", "arrow", "jsonlite", "htmlwidgets", "ggplot2", "plotly", "BiocManager", "remotes"))
+install.packages(c("devtools", "arrow", "jsonlite", "htmlwidgets", "ggplot2", "plotly", "BiocManager"))
 
 BiocManager::install("preprocessCore", configure.args = c(preprocessCore = "--disable-threading"), force=TRUE, update=TRUE, type = "source")
 BiocManager::install(c("minfi", "Rsamtools", "RnBeads", "illuminaio", "nullranges"))
