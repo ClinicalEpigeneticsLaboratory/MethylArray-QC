0bb051c9921baaead86bf5031398eaca40c5ac33
Patrycja Przybyłowicz <patrycja.przybylowicz@pum.edu.pl>
2025-04-18 14:22:39 +0200

    Batch effect evaluation figures: 1) added decorator call to figure generation logic, 2) removed unneccessary imports, 3) updated box layout so that 10 boxplots (current max) can be easily shown in a figure of the expected size (650 px x 650 px)

--- bin/batch_effect.py
+++ bin/batch_effect.py
@@ -1,14 +1,45 @@
 #!/usr/local/bin/python
 
+from decorators import update_and_export_plot
 import sys
-from pathlib import Path
-
+import plotly.graph_objects as go
 import pandas as pd
 import plotly.express as px
 
+def getSingleFig(
+    sample_sheet: pd.DataFrame,
+    grouped: pd.DataFrame,
+    row_num: int,
+    column: str,
+) -> go.Figure:
+    ids_to_plot = list(
+        sample_sheet.loc[sample_sheet["Plot_num"] == row_num, column]
+    )
 
-def getFigJson(
-    path_to_imputed_mynorm: str, path_to_sample_sheet: str, column: str
+    # Check if we have valid Sentrix_IDs to plot
+    if ids_to_plot:
+        # Selecting columns for the boxplot
+        grouped_row = grouped.loc[:, ids_to_plot]
+
+        # Reset the index to include CpG as a column
+        grouped_row_reset = grouped_row.reset_index(names="CpG")
+
+        # Melt the grouped row into long format for plotly
+        grouped_row_melted = grouped_row_reset.melt(
+            id_vars="CpG", var_name=column, value_name="Mean beta value"
+        )
+
+        fig = px.box(grouped_row_melted, x=column, y="Mean beta value")
+        fig.update_layout(boxgap=0.05)
+        fig.update_xaxes(tickangle = 90)
+        return fig
+    else:
+        print(f"Warning: No {column}s found for row {row_num}.")
+
+def getAllFigs(
+    path_to_imputed_mynorm: str, 
+    path_to_sample_sheet: str, 
+    column: str,
 ) -> None:
     # Load data
     imputed_mynorm = pd.read_parquet(path_to_imputed_mynorm)
@@ -28,33 +59,19 @@ def getFigJson(
     # Create figure
     grouped = data.groupby(column).mean().T
 
-    # the logic for subplot generation if there are more than 1 plots to generate
-    plot_rows = sample_sheet["Plot_num"].nunique()
-    plot_height = 600 * plot_rows
-
     for row_num in sample_sheet["Plot_num"].unique():
-        ids_to_plot = list(
-            sample_sheet.loc[sample_sheet["Plot_num"] == row_num, column]
+        fig = getSingleFig(
+            column = column, 
+            grouped = grouped, 
+            row_num = row_num,
+            sample_sheet = sample_sheet,
         )
 
-        # Check if we have valid Sentrix_IDs to plot
-        if ids_to_plot:
-            # Selecting columns for the boxplot
-            grouped_row = grouped.loc[:, ids_to_plot]
-
-            # Reset the index to include CpG as a column
-            grouped_row_reset = grouped_row.reset_index(names="CpG")
-
-            # Melt the grouped row into long format for plotly
-            grouped_row_melted = grouped_row_reset.melt(
-                id_vars="CpG", var_name=column, value_name="Mean beta value"
-            )
-
-            fig = px.box(grouped_row_melted, x=column, y="Mean beta value")
-            fig.update_layout(width=600, height=plot_height, template="ggplot2")
-            fig.write_json(file=f"{row_num}.json", pretty=True)
-        else:
-            print(f"Warning: No {column}s found for row {row_num}.")
+        if fig is not None:
+            @update_and_export_plot(f"{row_num}.json")
+            def exportFig():
+                return fig
+            exportFig()
 
 
 def main():
@@ -68,7 +85,7 @@ def main():
     sample_sheet_path = sys.argv[2]
     col = str(sys.argv[3])
 
-    getFigJson(
+    getAllFigs(
         path_to_imputed_mynorm=imputed_mynorm_path,
         path_to_sample_sheet=sample_sheet_path,
         column=col,
