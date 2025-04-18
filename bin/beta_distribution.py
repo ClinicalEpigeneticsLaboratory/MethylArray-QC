f7f5fd4315305419a7ed4804a75ae558bb71e2dc
Patrycja Przybyłowicz <patrycja.przybylowicz@pum.edu.pl>
2025-04-18 14:37:26 +0200

    Beta distribution plot: updated figure appearance, modified figure generation logic (decorator included)

--- bin/beta_distribution.py
+++ bin/beta_distribution.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python
 
+from decorators import update_and_export_plot
+
 import sys
 from pathlib import Path
 
@@ -7,6 +9,25 @@ import numpy as np
 import pandas as pd
 import plotly.figure_factory as ff
 
+@update_and_export_plot(json_path = "beta_distribution.json", height = 400, showlegend = False)
+def getBetaDistrPlot(n_cpgs_beta_distr: int, plot_data: pd.DataFrame) -> ff:
+    # Prepare figure
+    plot_title = (
+        f"Density plot of imputed data<br>(randomly selected {n_cpgs_beta_distr} CpGs)"
+    )
+
+    fig = ff.create_distplot(
+        plot_data.to_numpy(),
+        group_labels=plot_data.index.to_list(),
+        show_hist=False,
+        show_rug=False,
+        curve_type="kde",
+    )
+    fig.update_xaxes(range=[0, 1], title="Beta")
+    fig.update_yaxes(title="Density")
+    fig.update_layout(title_text=plot_title, margin_t = 125)
+
+    return fig
 
 # I cannot add customized hover to this type of plot, may require more work or impossible:
 # https://stackoverflow.com/questions/62448872/plotly-how-to-modify-hovertemplate-of-a-histogram
@@ -34,28 +55,7 @@ def main():
     plot_data = imputed_mynorm.loc[cpgs_to_plot]
     plot_data = plot_data.T
 
-    # Prepare figure
-    plot_title = (
-        f"Density plot of imputed data (randomly selected {n_cpgs_beta_distr} CpGs)"
-    )
-
-    fig = ff.create_distplot(
-        plot_data.to_numpy(),
-        group_labels=plot_data.index.to_list(),
-        show_hist=False,
-        show_rug=False,
-        curve_type="kde",
-    )
-    fig.update_xaxes(range=[0, 1], title="Beta")
-    fig.update_yaxes(title="Density")
-    fig.update_layout(
-        width=600,
-        height=300,
-        template="ggplot2",
-        title_text=plot_title,
-        showlegend=False,
-    )
-    fig.write_json(file="beta_distribution.json", pretty=True)
+    getBetaDistrPlot(n_cpgs_beta_distr = n_cpgs_beta_distr, plot_data = plot_data)
 
 
 if __name__ == "__main__":
