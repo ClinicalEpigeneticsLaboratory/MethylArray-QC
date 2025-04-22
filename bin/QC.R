71ac10959de9373902634ec7113ff2cc846872e6
Patrycja Przybyłowicz <patrycja.przybylowicz@pum.edu.pl>
2025-04-22 15:42:13 +0200

    R code linting

--- bin/QC.R
+++ bin/QC.R
@@ -1,13 +1,13 @@
 #!/bin/Rscript
 
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
-if (length(args)!=3) {
+if (length(args) != 3) {
   stop("Expected input: Rscript QC.R <idats> <cpus> <sample_sheet>")
 } else {
-  idats = args[1]
-  cpus = args[2]
-  sample_sheet_path = args[3]
+  idats <- args[1]
+  cpus <- args[2]
+  sample_sheet_path <- args[3]
 }
 
 library(sesame)
@@ -23,10 +23,10 @@ sample_list_dir <- list()
 sample_list_dir <- file.path(idats, sample_sheet$Array_Position)
 
 sdfs <- lapply(sample_list_dir, FUN = readIDATpair, manifest = NULL, platform = "", min_beads = NULL, controls = NULL, verbose = FALSE)
-if(length(sdfs) < nrow(sample_sheet)) stop(paste0("IDATs for ", nrow(sample_sheet) - length(sdfs)), " samples are missing!")
+if (length(sdfs) < nrow(sample_sheet)) stop(paste0("IDATs for ", nrow(sample_sheet) - length(sdfs)), " samples are missing!")
 names(sdfs) <- sample_sheet$Sample_Name
 
-qcs <- openSesame(sdfs, prep="", func=sesameQC_calcStats, BPPARAM = BiocParallel::MulticoreParam(cpus))
+qcs <- openSesame(sdfs, prep = "", func = sesameQC_calcStats, BPPARAM = BiocParallel::MulticoreParam(cpus))
 
 message("QC ...")
 quality_metrics <- do.call(rbind, lapply(qcs, as.data.frame))
