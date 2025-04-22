675e0d13eef045ac578c35d66c5606daf0c5eef2
Patrycja Przybyłowicz <patrycja.przybylowicz@pum.edu.pl>
2025-04-22 15:45:23 +0200

    R code linting

--- requirements_sesame.R
+++ requirements_sesame.R
@@ -2,6 +2,6 @@ options(Ncpus = 10)
 
 install.packages(c("devtools", "arrow", "jsonlite", "htmlwidgets", "ggplot2", "plotly", "BiocManager"))
 
-BiocManager::install("preprocessCore", configure.args = c(preprocessCore = "--disable-threading"), force=TRUE, update=TRUE, type = "source")
+BiocManager::install("preprocessCore", configure.args = c(preprocessCore = "--disable-threading"), force = TRUE, update = TRUE, type = "source")
 BiocManager::install(c("minfi", "Rsamtools", "RnBeads", "illuminaio", "nullranges"))
 BiocManager::install("sesame")
